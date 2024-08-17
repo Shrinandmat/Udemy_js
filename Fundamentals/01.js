14d480b4d6a22622737d9b864a78533dc6f2c877
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-17 08:25:38 +0000

    commit

--- Fundamentals/01.js
+++ Fundamentals/01.js
@@ -1,1 +1,42 @@
 console.log("Hello World");
+
+
+$name = "Shrinand";
+
+console.log($name); // we can use $
+
+let PI = 3.1415; // Use uppercase for real constants
+
+
+let country = 'Portugal';
+let continent = 'Europe';
+let population = 10;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// There are 7 primitive datatypes
+
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+
+let isJavascriptFun = true;
+console.log(isJavascriptFun);
+console.log(typeof 34);
+console.log(typeof "Name");
+console.log(typeof null);
+console.log(typeof undefined);
+sym = Symbol("YES");
+console.log(sym);
+n = BigInt('1223');
+console.log(n);
+
+
+
+
