c7810a1c5f28dee25e38a9f3393804e18b713c07
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-17 15:47:26 +0000

    commit changes done

---
+++ Fundamentals2/01.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let hasDriverLicence = false;
+const passTest = true;
+
+// if(passTest) hasDriversLicence = true;
+// After using strict mode you can see the errors on console
+if(passTest) hasDriverLicence = true;
+
+if(hasDriverLicence) console.log('I can drive car. : D')
+
+const private = 534;
+
+function Fruitprocessor(apples , oranges){
+    console.log(apples,oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+
+
+}
+
+Fruitprocessor(2,3);
