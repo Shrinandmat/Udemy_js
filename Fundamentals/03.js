ff6cf8df711b36730c2a16b63d1f7cad39d169b4
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-17 13:33:57 +0000

    commit

---
+++ Fundamentals/03.js
@@ -0,0 +1,26 @@
+const hasDriverlicence = true;
+const hasGoodvision = true;
+
+console.log(hasDriverlicence && hasGoodvision);
+console.log(hasDriverlicence || hasGoodvision);
+
+const shouldDrive = hasDriverlicence && hasGoodvision;
+
+if(shouldDrive){
+    console.log('Sarah is able to Drive');
+
+}else{
+    console.log('Sarah is not able to drive');
+}
+const isTired = true;
+
+console.log(hasDriverlicence && hasGoodvision && !isTired){
+    console.log(`Sarah is able to drive`);
+};
+
+const scoreDolphins = (96 + 108 + 89)/3;
+const scoreKoalas = (81 + 98 + 101)/3;
+
+console.log(scoreDolphins,scoreKoalas);
+
+
