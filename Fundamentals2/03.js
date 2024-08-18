f77120b39147de63b5cbc11859d3608ddbd51d6e
Shrinandmat <141657648+Shrinandmat@users.noreply.github.com>
2024-08-18 11:30:01 +0000

    commit changes done

--- Fundamentals2/03.js
+++ Fundamentals2/03.js
@@ -1,4 +1,20 @@
-çonst jonas = {
+// Object Function
+
+const profile = {
     firstname : 'Jonas',
-    
+     lastName : 'Shmidtmann',
+     age : 23,
+     birthyear : 2002 ,
+     calcAge : function(birthyear) {
+        return 2037 - birthyear;
+     }
 }
+
+
+calcAge = function(birthyear) {
+   return 2037 - birthyear;
+}
+
+console.log(profile.calcAge(1991));
+console.log(profile['calcAge'](1991));
+
